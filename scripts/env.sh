df5c7c8b0f106f2f5a40e677a07745c6eca3f835
Shmpei Wakabayashi <pika.waka.0621@gmail.com>
2021-03-05 15:21:14 +0900

    update env.sh

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
 # shellcheck source=/dev/null
-source "$HOME/ros/vive_ws/devel/setup.bash"
+if [ -e "$HOME/ros/baxter_ws/" ]; then
+  source "$HOME/ros/baxter_ws/devel/setup.bash"
+fi
+if [ -e "$HOME/ros/vive_ws/" ]; then
+  source "$HOME/ros/vive_ws/devel/setup.bash"
+fi
 exec "$@"
